061fc176b8f851b5897cc4f53a8ff4bdf74d7784
Atimscreatives <96551900+Atimscreatives@users.noreply.github.com>
2026-02-20 04:10:01 +0100

    Update sitemap.routes.js

--- routes/sitemap.routes.js
+++ routes/sitemap.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 import { generateSitemap } from "../controllers/sitemap.controller.js";
 
 router.get("/sitemap.xml", generateSitemap);
+router.get("/sitemapnew.xml", generateSitemap);
 
 export default router;
